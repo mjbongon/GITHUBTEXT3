17f2f05a8bb502eb7d824954cb7624b9a4d4697c
mjbongon <mjgbongon@gmail.com>
2023-05-22 19:25:41 +0200

    master

--- src/Main3.java
+++ src/Main3.java
@@ -1,2 +1,34 @@
 public class Main3 {
+
+    private String name;
+    private int id;
+    private double mj;
+
+    public Main3 (int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getMj() {
+        return mj;
+    }
+
+    public void setMj(double mj) {
+        this.mj = mj;
+    }
 }
